8cdf2e9dccea79e4e0fd75f9ca463360d7aa977b
Otávio Pace <otaviopp8@gmail.com>
2018-02-23 14:04:59 -0300

    neander_emu: remove bitshifting and add tests
    - Remove bitshifting in favor of masking
    - Read byte from instance instead of receiving by argument
    - Add tests to byte_to_opcode method of Parser

--- neander_emu/src/tests.rs
+++ neander_emu/src/tests.rs
@@ -7,3 +7,57 @@ fn test_next() {
 
     assert_eq!(parser.next(), Some(0x61));
 }
+
+#[test]
+fn byte_to_opcode_when_nop() {
+    let file = File::open("binary_files/nop.dat").unwrap();
+    let mut parser = Parser::new(file);
+
+    let opcode = parser.byte_to_opcode()
+        .expect("should return NOP opcode");
+
+    match opcode {
+        OpCode::NOP => (),
+        _ => panic!("should be NOP OpCode"),
+    };
+}
+
+#[test]
+fn byte_to_opcode_when_sta() {
+    let file = File::open("binary_files/sta.dat").unwrap();
+    let mut parser = Parser::new(file);
+
+    let opcode = parser.byte_to_opcode()
+        .expect("should return STA opcode");
+
+    match opcode {
+        OpCode::STA(operand) => (),
+        _ => panic!("should be STA OpCode"),
+    };
+}
+
+#[test]
+fn byte_to_opcode_when_invalid_opcode() {
+    let file = File::open("binary_files/non_existing_opcode.dat").unwrap();
+    let mut parser = Parser::new(file);
+
+    let result = parser.byte_to_opcode();
+
+    match result {
+        Err(ParserError::InvalidOpCode(operand)) => (),
+        _ => panic!("should not match an opcode"),
+    };
+}
+
+#[test]
+fn byte_to_opcode_when_missing_operand() {
+    let file = File::open("binary_files/lda_missing_operand.dat").unwrap();
+    let mut parser = Parser::new(file);
+
+    let result = parser.byte_to_opcode();
+
+    match result {
+        Err(ParserError::MissingOperand(opcode)) => (),
+        _ => panic!("should not match an operand"),
+    };
+}
