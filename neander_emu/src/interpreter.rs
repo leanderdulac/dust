d3ed08dadaa23deecf85a3ed2c0618e71cdf5ce3
Otávio Pace <otaviopp8@gmail.com>
2018-02-28 14:03:05 -0300

    neander_emu: add sta function to interpreter

--- neander_emu/src/interpreter.rs
+++ neander_emu/src/interpreter.rs
@@ -1,2 +1,6 @@
 pub fn nop() {
 }
+
+pub fn sta(address: &mut u8, accumulator: u8) {
+   *address = accumulator;
+}
--- neander_emu/src/tests/interpreter.rs
+++ neander_emu/src/tests/interpreter.rs
@@ -4,3 +4,13 @@ use super::super::interpreter::*;
 fn test_nop() {
     nop();
 }
+
+#[test]
+fn test_sta() {
+    let mut value: u8 = 0x00;
+    let mut accumulator: u8 = 0xa0;
+
+    sta(&mut value, accumulator);
+
+    assert_eq!(value, 0xa0);
+}
