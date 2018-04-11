ba84e65f7791d61425f84bccec97fd970524e24e
Otávio Pace <otaviopp8@gmail.com>
2018-04-11 07:25:42 -0300

    wip: rust is stupid

--- neander_emu/src/lib.rs
+++ neander_emu/src/lib.rs
@@ -11,6 +11,12 @@ pub struct Add<'a> {
     address: &'a u8,
 }
 
+impl<'a> Add<'a> {
+    fn new(address: &'a u8) -> Add {
+        Add { address }
+    }
+}
+
 impl<'a> Instruction for Add<'a> {
     fn execute(&self, state: State) -> State {
         let new_accumulator: u8 = state.accumulator + *self.address;
@@ -24,8 +30,64 @@ impl<'a> Instruction for Add<'a> {
 pub struct Nop {
 }
 
+impl Nop {
+    fn new() -> Nop {
+        Nop {}
+    }
+}
+
 impl Instruction for Nop {
     fn execute(&self, state: State) -> State {
         state
     }
+}
+
+pub struct Halt {
+}
+
+impl Halt {
+    fn new() -> Halt {
+        Halt {}
+    }
+}
+
+impl Instruction for Halt {
+    fn execute(&self, state: State) -> State {
+        state
+    }
+}
+
+use std::error::Error;
+use std::fs::File;
+use std::io::Bytes;
+
+pub fn parser(mut bytes: Bytes<File>) -> Result<Vec<Box<Instruction>>, Box<Error>> {
+    let mut instructions: Vec<Box<Instruction>> = Vec::with_capacity(10);
+
+    // let mut opcode;
+    loop {
+        match bytes.next() {
+            Some(byte_result) => match byte_result {
+                Ok(byte) => {
+                    println!("byte {:x}", byte);
+                    match byte {
+                        0x00 => instructions.push(Box::new(Nop::new())),
+                        0x30 => instructions.push(Box::new(Add::new(&0))),
+                        _ => break
+                    };
+
+                },
+                Err(err) => {
+                    println!("err");
+                },
+            },
+            None => {
+                println!("None");
+                instructions.push(Box::new(Halt::new()));
+                break
+            },
+        };
+    };
+
+    Ok(instructions)
 }
--- neander_emu/src/main.rs
+++ neander_emu/src/main.rs
@@ -1,3 +1,16 @@
+extern crate neander_emu;
+
+use std::fs::File;
+use std::io::Read;
+use neander_emu::parser;
+
 fn main() {
     println!("Hello neander rewrite!");
+    let file = File::open("binary_files/nop_add").unwrap();
+    let instructions = parser(file.bytes());
+
+    for instruction in instructions {
+        println!("yo");
+
+    }
 }
